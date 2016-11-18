9241daf150c14fb6d6ceccab9a17b845d828d6b2
Halil İbrahim Yaşarsoy <hyasarsoy@gmail.com>
2016-11-18 20:05:38 +0200

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,20 +1,23 @@
-import fresh_tomatoes
-import media
+import fresh_tomatoes #import the file, file inside the html code :) 
+import media #import the mediapy
 
-finding_nemo = media.Movie("Finding Nemo",
-                        "Toys Come to Life",
-                        "https://upload.wikimedia.org/wikipedia/en/2/29/Finding_Nemo.jpg",
-                        "https://www.youtube.com/watch?v=SPHfeNgogVs")
+#define the Movie
+finding_nemo = media.Movie("Finding Nemo", #Movie Name
+                        "Nemo", #Description
+                        "https://upload.wikimedia.org/wikipedia/en/2/29/Finding_Nemo.jpg", #İmage
+                        "https://www.youtube.com/watch?v=SPHfeNgogVs") #youtube link
+
+
 
 school_of_rock = media.Movie("School of Rock",
-                        "Toys Come to Life",
+                        "School",
                         "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
                         "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
 
 hermano = media.Movie("Hermano",
-                        "Toys Come to Life",
+                        "Hermanoe",
                         "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQwNDEzNzg1N15BMl5BanBnXkFtZTcwMDgwODk1OA@@._V1_SY1000_CR0,0,679,1000_AL_.jpg",
                         "https://www.youtube.com/watch?v=B7c3NcKzSRQ")
 
-movies = [finding_nemo, school_of_rock, hermano]
+movies = [finding_nemo, school_of_rock, hermano] #write to all movie
 fresh_tomatoes.open_movies_page(movies)
