320e9092a4fbfd76a64ccbca262e5accfbfd42ca
Halil İbrahim Yaşarsoy <hyasarsoy@gmail.com>
2016-11-27 15:46:50 +0200

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -8,16 +8,18 @@ finding_nemo = media.Movie("Finding Nemo", #Movie Name
                         "https://www.youtube.com/watch?v=SPHfeNgogVs") #youtube link
 
 
-
+#define the Movie
 school_of_rock = media.Movie("School of Rock",
                         "School",
                         "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
                         "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
 
+#define the Movie
 hermano = media.Movie("Hermano",
                         "Hermanoe",
                         "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQwNDEzNzg1N15BMl5BanBnXkFtZTcwMDgwODk1OA@@._V1_SY1000_CR0,0,679,1000_AL_.jpg",
                         "https://www.youtube.com/watch?v=B7c3NcKzSRQ")
 
 movies = [finding_nemo, school_of_rock, hermano] #write to all movie
-fresh_tomatoes.open_movies_page(movies)
+fresh_tomatoes.open_movies_page(movies) #its html page you can check this file
+
