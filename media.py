93eac4ba24968da1a236e7e96744106410efc7a3
Halil İbrahim Yaşarsoy <hyasarsoy@gmail.com>
2016-11-29 23:39:34 +0200

    Update media.py

--- media.py
+++ media.py
@@ -1,9 +1,9 @@
-import webbrowser #import browser
+iimport webbrowser #import browser
 
-#define to class class inculude the movie detail, 
-class Movie (): #class is start
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube): #define the movie information
-        self.title = movie_title #Movie title
-        self.storyline = movie_storyline #Story line
-        self.poster_image_url = poster_image #İmage 
+#define to class class inculude the movie detail, define the movie information
+class Movie ():
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube): 
+        self.title = movie_title #movie title
+        self.storyline = movie_storyline #story line
+        self.poster_image_url = poster_image #image 
         self.trailer_youtube_url = trailer_youtube #trailer video
